d036b7195b886f8134aa59579d0a02ca475357d4
Nick Berens <nickberens360@gmail.com>
2025-05-18 07:02:53 -0500

    update tsconfig to squash meta glob error

--- src/plugins/component-documentation/index.ts
+++ src/plugins/component-documentation/index.ts
@@ -19,8 +19,8 @@ export default {
 };
 
 function convertPathToExampleName(path: string): string {
-  return 'Example' + path.replaceAll(' ', '-')
-    .replaceAll('.vue', '')
+  return 'Example' + path.replace(/ /g, '-')
+    .replace(/\.vue/g, '')
     .split('/')
     .map((part) => {
       return part.split('-')
